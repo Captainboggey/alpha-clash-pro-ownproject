729d526c704b7541f2a96d1b20e701fe5b2b97b5
captainboogey <anik35-700@diu.edu.bd>
2024-10-25 00:16:56 +0600

    keyboard color and inputed text added and randow number also

--- script/alpha.js
+++ script/alpha.js
@@ -1,1 +1,27 @@
-console.log('hello from js')
+function addClassName(elementId){
+    const enterId = document.getElementById(elementId);
+    enterId.classList.add('hidden');
+
+}
+function removeClassName(elementId){
+    const enterId= document.getElementById(elementId);
+    enterId.classList.remove('hidden');
+}
+
+function randomNumber(){
+    const randomAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabets =randomAlphabet.split('');
+    console.log(alphabets);
+    const randomNumberGenerator = Math.random()*25;
+    const roundNumber = Math.round(randomNumberGenerator);
+    const alphabet =alphabets[roundNumber];
+    const inputedAlpha =document.getElementById('inputed-alpha');
+    inputedAlpha.innerText=alphabet;
+    
+    return alphabet;
+}
+
+function setBackgorundColor(elementId){
+    const inputedText = document.getElementById(elementId);
+    inputedText.classList.add('bg-amber-300');
+}
---
+++ script/play.js
@@ -0,0 +1,9 @@
+function playNow(){
+    addClassName('home-section');
+    removeClassName('playground-section');
+    randomNumber();
+    const inputed = randomNumber();
+    setBackgorundColor(inputed);
+   
+
+}
